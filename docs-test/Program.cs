f6fabab052357542e23d3a56556c92e7a516233a
stephenwilliams <saw@slpe.com>
2024-07-05 13:25:20 +0100

    push test

--- docs-test/Program.cs
+++ docs-test/Program.cs
@@ -5,7 +5,7 @@ class Program
     
     /// <summary>
     /// This is the main method of the program.
-    /// It prints "Hello, World!" to the console.
+    /// It prints "Hello, World!" to the console. 
     /// </summary>
     /// <param name="args">These are the arguments to the main method.</param>
     static void Main(string[] args)
