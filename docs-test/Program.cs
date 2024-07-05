b39b4e3a208fd148e7c98f7a929f0155cd074fb2
stephenwilliams <saw@slpe.com>
2024-07-05 08:49:10 +0100

    First commit for docs test repo

---
+++ docs-test/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace docs_test;
+
+class Program
+{
+    
+    /// <summary>
+    /// This is the main method of the program.
+    /// It prints "Hello, World!" to the console.
+    /// </summary>
+    /// <param name="args">These are the arguments to the main method.</param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
