353e35066a8c1a2f596920080ba4f66acec1ea92
stephenwilliams <saw@slpe.com>
2024-07-08 09:12:46 +0100

    another try

---
+++ docs-test/TestClass.cs
@@ -0,0 +1,13 @@
+namespace docs_test;
+
+public class TestClass
+{
+    /// <summary>
+    /// A test method to do nothing 
+    /// </summary>
+    /// <param name="test">A test input parameter that does nothing</param>
+    public void Test(int test)
+    {
+        // do nothing
+    }
+}
